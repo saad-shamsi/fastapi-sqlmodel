4859ddbf9795f0f5e6774c61a68eb2cef53efc4a
Saad Shamsi <114074040+saad-shamsi@users.noreply.github.com>
2024-02-08 21:45:51 +0500

    updated

--- _backconfig.py
+++ _backconfig.py
@@ -2,7 +2,7 @@
 from sqlmodel import create_engine, Session
 from fastapi import FastAPI
 
-connection_string = "postgresql://saadshamsi13:hrpGdD8As6IZ@ep-bitter-sunset-a1x6gnrg.ap-southeast-1.aws.neon.tech/anew?sslmode=require"
+connection_string = "add connnection string here"
 
 engine = create_engine(connection_string)
 
